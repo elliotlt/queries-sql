812753878814b0b7e028876f42b7c9fb2ce7590d
elliotlt <62412790+elliotlt@users.noreply.github.com>
2025-02-17 12:00:37 +0100

    Update and rename price-list-for-users-wo_atc.sql to price-list-for-users-wo-atc.sql

--- price-list-for-users-wo-atc.sql
+++ price-list-for-users-wo-atc.sql
@@ -7,7 +7,7 @@ SELECT
     item_list_index,  -- Position of the item in the list
     price_range,      -- Predefined price range category of the item
     user_pseudo_id    -- Unique identifier for the user session
-FROM ANALYTICS.DM_COMMON.GA4_ITEMS_CS vi
+FROM ga4_table vi
 WHERE vi.event_name = 'view_item_list'  -- Filtering only "view_item_list" event (user saw the list)
     /* Ensuring the date range is correctly set */
     AND vi.event_date >= '20240101'  -- Adjusted to YYYYMMDD format to work correctly with filtering
@@ -18,7 +18,7 @@ WHERE vi.event_name = 'view_item_list'  -- Filtering only "view_item_list" event
     */
     AND NOT EXISTS (
         SELECT 1
-        FROM ANALYTICS.DM_COMMON.GA4_ITEMS_CS atc
+        FROM ga4_table atc
         WHERE atc.event_name = 'add_to_cart'  -- Looking for users who added an item to the cart
             AND atc.event_date >= '20240101'  -- Ensuring the same date range is used
             AND atc.user_pseudo_id = vi.user_pseudo_id  -- Matching users between the main query and the subquery
