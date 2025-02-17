bbc4743c00429ca418bf73551085ac5b494213fa
elliotlt <62412790+elliotlt@users.noreply.github.com>
2025-02-17 11:43:42 +0100

    Update landing-page-conversion-rate.sql

--- landing-page-conversion-rate.sql
+++ landing-page-conversion-rate.sql
@@ -4,8 +4,8 @@
 */
 WITH landing AS (
     SELECT 
-        visit_date,  -- Date of the visit
-        unique_session_id,  -- Unique session identifier
+        visit_date,  
+        unique_session_id,  
         /* Extract the base URL of the landing page by removing any query parameters */
         IFF(CHARINDEX('?', page_location) > 0, 
             LEFT(page_location, CHARINDEX('?', page_location, 0) - 1), 
@@ -20,8 +20,8 @@ WITH landing AS (
 */
 conversions AS (
     SELECT 
-        visit_date,  -- Date of the visit
-        unique_session_id  -- Unique session identifier
+        visit_date,  -
+        unique_session_id  
     FROM ga4_table
     WHERE event_name = 'purchase' -- Only considering purchase events
 )
@@ -30,13 +30,13 @@ conversions AS (
    Main query to aggregate sessions and purchases per landing page and visit date.
 */
 SELECT
-    lp.landingPage,  -- Extracted landing page URL
-    lp.visit_date,  -- Date of visit
-    COUNT(DISTINCT lp.unique_session_id) AS total_sessions,  -- Total unique sessions per landing page
-    COUNT(DISTINCT cv.unique_session_id) AS purchase  -- Count of unique sessions that resulted in a purchase
+    lp.landingPage, 
+    lp.visit_date,  
+    COUNT(DISTINCT lp.unique_session_id) AS total_sessions,  
+    COUNT(DISTINCT cv.unique_session_id) AS purchase  -
 FROM landing AS lp
 LEFT JOIN conversions AS cv
     /* Joining on unique session ID and visit date to track which sessions led to purchases */
     ON lp.unique_session_id = cv.unique_session_id
     AND lp.visit_date = cv.visit_date
-GROUP BY lp.landingPage, lp.visit_date;  -- Grouping by landing page and visit date
+GROUP BY lp.landingPage, lp.visit_date;  
